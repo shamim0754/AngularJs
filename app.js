289598fc4922e7c8d65d75b520933ce2043db686
Md.Shamim Miah <shamim.ict0754@gmail.com>
2016-01-31 14:28:41 +0600

    angular route adding

--- app.js
+++ app.js
@@ -3,5 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+var app = angular.module('leaveApp', [
+    'ngRoute'
+]);
+  
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/welcome', {
+              templateUrl: 'views/welcome.html',
+              controller: 'SimpleController'
+            }).
+            when('/applyLeave', {
+              templateUrl: 'views/applyLeave.html',
+              controller: 'SimpleController'
+            }).
+            otherwise({
+              redirectTo: '/welcome'
+            });
+  }]);
 
